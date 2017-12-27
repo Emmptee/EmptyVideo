cddb3e31923b30d4cab4fa86bb91900642ec0cc4
shidongfang <shidongfang@oradt.com>
2017-12-27 18:05:32 +0800

    double C

--- app/src/main/java/com/douglas/videolive/view/common/activity/PhoneLiveVideoActivity.java
+++ app/src/main/java/com/douglas/videolive/view/common/activity/PhoneLiveVideoActivity.java
@@ -1,5 +1,6 @@
 package com.douglas.videolive.view.common.activity;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.PointF;
@@ -7,6 +8,8 @@ import android.graphics.drawable.BitmapDrawable;
 import android.media.AudioManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.provider.Settings;
 import android.support.v4.content.res.ResourcesCompat;
 import android.util.Pair;
@@ -32,7 +35,6 @@ import com.douglas.videolive.ui.DivergeView;
 import com.douglas.videolive.ui.loadplay.LoadingView;
 import com.douglas.videolive.view.common.fragment.MainDialogFragment;
 
-import java.security.Provider;
 import java.util.ArrayList;
 
 import butterknife.BindView;
@@ -61,7 +63,7 @@ public class PhoneLiveVideoActivity extends BaseActivity<CommonPhoneLiveVideoMod
     @BindView(R.id.iv_control_img)
     ImageView ivControlImg;
     @BindView(R.id.tv_control_name)
-    TextView tvCotrolName;
+    TextView tvControlName;
     @BindView(R.id.tv_control)
     TextView tvControl;
     @BindView(R.id.control_center)
@@ -78,17 +80,45 @@ public class PhoneLiveVideoActivity extends BaseActivity<CommonPhoneLiveVideoMod
     private String room_id;
     private String imgUrl;
     private SVProgressHUD svProgressHUD;
-    /*音量、亮度*/
-    private int mScreenWidth = 0;
-    private AudioManager mAudioManager;
-    private int mMaxVolume;
-    private int mShowVolume;
-    private int mShwoLightness;
     private DanmuProcess mDanmuProcess;
     private ArrayList<Bitmap> mList;//点赞的动画
     private int mIndex = 0;
+    /*声音*/
+    public final static int ADD_FLAG = 1;
+    /*亮度*/
+    public final static int SUB_FLAG = -1;
+
+    /*音量、亮度*/
+    private int mScreenWidth = 0;
+    private AudioManager mAudioManager;
+    private int mMaxVolume;//最大音量
+    private int mShowVolume;//当前音量
+    private int mShowLightness;//当前亮度
+
     private GestureDetector mGestureDetector;
     private GestureDetector.SimpleOnGestureListener mSimpleOnGestureListener;
+    private boolean mIsFullScreen;
+
+
+    public static final int HIDE_CONTROL_BAR = 0x02;//隐藏控制条
+    public static final int HIDE_TIME = 5000;//隐藏控制条时间
+    public static final int SHOW_CENTER_CONTROL = 0x03;//显示中间控制
+    public static final int SHOW_CONTROL_TIME = 1000;
+    @SuppressLint("HandlerLeak")
+    private Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case HIDE_CONTROL_BAR://隐藏top、bottom
+                    break;
+                case SHOW_CENTER_CONTROL://隐藏center控件
+                    if (controlCenter != null) {
+                        controlCenter.setVisibility(View.GONE);
+                    }
+                    break;
+            }
+        }
+    };
 
 
     @Override
@@ -200,7 +230,7 @@ public class PhoneLiveVideoActivity extends BaseActivity<CommonPhoneLiveVideoMod
         mAudioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
         mMaxVolume = mAudioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
         mShowVolume = mAudioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
-        mShwoLightness = getScreenBrightness();
+        mShowLightness = getScreenBrightness();
     }
 
     /**
@@ -222,12 +252,84 @@ public class PhoneLiveVideoActivity extends BaseActivity<CommonPhoneLiveVideoMod
     }
 
     private void addTouchListener() {
-        mSimpleOnGestureListener = new GestureDetector.SimpleOnGestureListener(){
+        mSimpleOnGestureListener = new GestureDetector.SimpleOnGestureListener() {
             @Override
             public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
-                return super.onScroll(e1, e2, distanceX, distanceY);
+                if (!mIsFullScreen) {//非全屏不进行手势操作
+                    return false;
+                }
+                float x1 = e1.getX();
+                float absDistanceX = Math.abs(distanceX);//distanceX < 0 从左到右
+                float absDistanceY = Math.abs(distanceY);//distanceY < 0 从上到下
+
+                //Y方向的距离比X方向的大，就是上下滑动
+                if (absDistanceX < distanceY) {
+                    if (distanceY > 0) {//向上滑动
+                        if (x1 >= mScreenWidth * 0.65) {//右侧调节音量
+                            changeVolume(ADD_FLAG);
+                        }else {//调节亮度
+                            changeLightness(ADD_FLAG);
+                        }
+                    }else {//向下滑动
+                        if (x1>=mScreenWidth *0.65){
+                            changeVolume(SUB_FLAG);
+                        }else {
+                            changeLightness(SUB_FLAG);
+                        }
+                    }
+                }else {//x方向的距离比Y方向的大
+
+                }
+                return false;
             }
+            //双击事件，有的视频播放器支持双击暂停，在这实现
         };
+    }
+
+    /**
+     * 调节亮度
+     * @param addFlag
+     */
+    private void changeLightness(int addFlag) {
+        mShowLightness += addFlag;
+        if (mShowLightness > 255) {
+            mShowLightness = 255;
+        } else if (mShowLightness <= 0) {
+            mShowLightness = 0;
+        }
+        tvControlName.setText("亮度");
+        ivControlImg.setImageResource(R.drawable.img_light);
+        tvControl.setText(mShowLightness * 100 / 255 + "%");
+        WindowManager.LayoutParams lp = getWindow().getAttributes();
+        lp.screenBrightness = mShowLightness / 255f;
+        getWindow().setAttributes(lp);
+
+        mHandler.removeMessages(SHOW_CENTER_CONTROL);
+        controlCenter.setVisibility(View.VISIBLE);
+        mHandler.sendEmptyMessageDelayed(SHOW_CENTER_CONTROL, SHOW_CONTROL_TIME);
+
+    }
+
+    /**
+     * 调整音量
+     *
+     * @param addFlag
+     */
+    private void changeVolume(int addFlag) {
+        mShowVolume += addFlag;
+        if (mShowVolume > 100) {
+            mShowVolume = 100;
+        } else if (mShowVolume < 0) {
+            mShowVolume = 0;
+        }
+        tvControlName.setText("音量");
+        ivControlImg.setImageResource(R.drawable.img_volume);
+        tvControl.setText(mShowVolume + "%");
+        int tagVolume = mShowVolume * mMaxVolume / 100;//音量的绝对值
+        mAudioManager.setStreamVolume(AudioManager.STREAM_MUSIC, tagVolume, 0);
+        mHandler.removeMessages(SHOW_CENTER_CONTROL);
+        controlCenter.setVisibility(View.VISIBLE);
+        mHandler.sendEmptyMessageDelayed(SHOW_CENTER_CONTROL, SHOW_CONTROL_TIME);
     }
 
     @Override
